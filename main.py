f8439c886e3077bb4545b08ea9b071f44bc402d3
Itay Marom <itaymarom07@gmail.com>
2022-09-19 19:55:03 +0300

    Add assignment numer 5

--- main.py
+++ main.py
@@ -71,9 +71,34 @@ if number == 2:
 elif number % 2 == 0:
     print(' ' * (number // 2 - 1) + str(number // 2))
 
-
 for i in range(number // 2 - 1, - 1, -1):
     line = '' + ' ' * i
     for num in range(i + 1, number + 1 - i):
         line += str(num)
     print(line)
+print("\n")
+
+# Assignment 5
+students_five_units = 0
+for student in range(1, 976):
+    print(f"\nHello student number {student}!")
+    name = input("Enter your name: ").title()
+    while True:
+        try:
+            units = int(input("Enter the number of units you are learning: "))
+            grade = int(input("Enter your grade in this class: "))
+        except ValueError:
+            print("The grade and units must be an integer!\n")
+        else:
+            if 3 <= units <= 5 and 0 <= grade <= 100:
+                if units == 5:
+                    students_five_units += 1
+                break
+            else:
+                print("The numbers you entered are invalid!\n")
+
+    print(f"The name of the student is {name}, he is in {units} units, and his grade in this class is {grade}.")
+    if grade > 95:
+        print("He got an A!")
+
+print(f"\nThe number of students in 5 units is {students_five_units}!")
